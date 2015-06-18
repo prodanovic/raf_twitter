ec929dc81b7ff5f054a3fa6ef11283722784e1f2
prodanovic <srdjanp@ballab.com>
2015-06-18 14:24:40 +0200

    started web part

---
+++ src/main/java/rs/raf/webprog/beans/LoginBean.java
@@ -0,0 +1,31 @@
+package rs.raf.webprog.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
